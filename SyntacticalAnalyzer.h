b756581e60c8f39d80e9f2d72eab4f6696b8358c
willmclaughlin13 <willmclaughlin13@gmail.com>
2019-12-06 22:07:47 -0800

    Update SyntacticalAnalyzer.h
    Added my parentheses counter.

--- SyntacticalAnalyzer.h
+++ SyntacticalAnalyzer.h
@@ -1,10 +1,3 @@
-//
-// Created by Will on 12/5/2019.
-//
-
-#ifndef CODEGENERATOR_SYNTACTICALANALYZER_H
-#define CODEGENERATOR_SYNTACTICALANALYZER_H
-
 /*******************************************************************************
 * File name:                                                                   *
 * Project: CS 460 Project 2 Fall 2019                                          *
@@ -25,34 +18,32 @@ using namespace std;
 
 class SyntacticalAnalyzer
 {
-public:
-    SyntacticalAnalyzer (char * filename);
-    ~SyntacticalAnalyzer ();
-private:
-    LexicalAnalyzer * lex;
+    public:
+	SyntacticalAnalyzer (char * filename);
+	~SyntacticalAnalyzer ();
+    private:
+	LexicalAnalyzer * lex;
     CodeGen * code;
-    token_type token;
-    string cppString;
-    int program ();
-    int more_defines ();
-    int define ();
-    int stmt_list ();
-    int stmt ();
-    int literal ();
-    int quoted_lit ();
-    int more_tokens ();
-    int param_list ();
-    int else_part ();
-    int stmt_pair ();
-    int stmt_pair_body ();
-    int action ();
-    int any_other_token ();
-    ofstream listing;
-    ofstream p2;
-    ofstream debug;
+	token_type token;
+	int program ();
+	int more_defines ();
+	int define ();
+	int stmt_list ();
+	int stmt ();
+	int literal ();
+	int quoted_lit ();
+	int more_tokens ();
+	int param_list ();
+	int else_part ();
+	int stmt_pair ();
+	int stmt_pair_body ();
+	int action ();
+	int any_other_token ();
+  int parentCount; // This is when we're inside a statement that heas
+                   // nested statements and we want a comma
+	ofstream listing;
+	ofstream p2;
+	ofstream debug;
 };
 
 #endif
-
-
-#endif //CODEGENERATOR_SYNTACTICALANALYZER_H
